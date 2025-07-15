0b3d9881164ddaa74d86a53818e44a3a30d3366c
rel700 <rel@mail.com>
2025-07-15 18:05:00 +0300

    Fix import statement for rootIndex in server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import router from './routes/rootIndex.js';
+import rootIndex from './routes/rootIndex.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/', router)
+app.use('/', rootIndex)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
