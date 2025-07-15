012a5978440c9982dd1abb277e79552a204c60f5
rel700 <rel@mail.com>
2025-07-16 00:31:16 +0300

    Add auth and products routers in server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import rootIndex from './routes/rootIndex.js';
+import productsRouter from './routes/products.js';
+import authRouter from './routes/auth.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/', rootIndex)
+app.use('/', rootIndex);
+app.use('/', authRouter);
+app.use('/products', productsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
